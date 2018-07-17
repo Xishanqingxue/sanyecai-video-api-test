cb2c1c4a5e266e9f468f036079eab93c307cefcf
gaoyinglong <gaoyinglong@kong.net>
2018-07-17 22:50:53 +0800

    add tools

---
+++ tools.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+
+if __name__ == '__main__':
+
+
+    while True:
+        module = int(input('''请输入要操作的类型序号:
+        
+        1、修改用户彩票账户资金
+        2、获取手机验证码
+        
+        '''))
+        
